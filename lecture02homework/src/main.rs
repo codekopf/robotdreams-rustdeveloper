1968702fbe13a67024e45a14822e791a5d9390a1
Andrej Buday <andrej.buday@gmail.com>
2023-10-09 21:45:40 +0200

    2023 10 09 Lecture 02 homework

---
+++ lecture02homework/src/main.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::io;
+use slug;
+
+// cargo run <action>
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: <program> <action>");
+        println!("Actions: lowercase, uppercase, no-spaces, slugify, sentence, unicode");
+        return;
+    }
+
+    let action = &args[1];
+    let mut input = String::new();
+
+    println!("Enter the text:");
+    io::stdin().read_line(&mut input).expect("Failed to read the input");
+    let input = input.trim();
+
+    if input.len() == 0 {
+        println!("Insufficient input length!")
+    }
+
+    match action.as_str() {
+        "lowercase" => {
+            println!("{}", input.to_lowercase());
+        }
+        "uppercase" => {
+            println!("{}", input.to_uppercase());
+        }
+        "no-spaces" => {
+            println!("{}", input.replace(" ", ""));
+        }
+        "slugify" => {
+            println!("{}", slug::slugify(input));
+        }
+        "sentence" => {
+            let mut chars = input.chars();
+            let first_char = chars.next().unwrap().to_uppercase().collect::<String>();
+            println!("{}{}.", first_char, chars.collect::<String>());
+        }
+        "unicode" => {
+            let codes: Vec<u32> = input.chars().map(|c| c as u32).collect();
+            for code in codes  {
+                print!("{}", code);
+            }
+        }
+        _ => {
+            println!("Invalid action! Use lowercase, uppercase, no-spaces, slugify, sentence or unicode.");
+        }
+    }
+
+}
